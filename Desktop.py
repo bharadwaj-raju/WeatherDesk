26e4136e87f03f3ffff7c8d50564b0f65a3b9a17
James Cowgill <james410@cowgill.org.uk>
2016-05-20 01:06:42 +0100

    Prefer XDG_CURRENT_DESKTOP over DESKTOP_SESSION

--- Desktop.py
+++ Desktop.py
@@ -25,15 +25,11 @@ def get_desktop_environment():
 
     else:
 
-        desktop_session = os.environ.get('DESKTOP_SESSION')
+        desktop_session = os.environ.get('XDG_CURRENT_DESKTOP')
 
         if desktop_session is None:
 
-            desktop_session = os.environ.get('XDG_CURRENT_DESKTOP')
-
-        elif desktop_session.lower() == 'default':
-
-            desktop_session = os.environ.get('XDG_CURRENT_DESKTOP')
+            desktop_session = os.environ.get('DESKTOP_SESSION')
 
         if desktop_session is not None:
 
